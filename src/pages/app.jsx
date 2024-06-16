75a42b345b1e73369a628019b58486ee16ba5988
Nikita Panchal <nikitapanchal4509@gmail.com>
2024-06-16 15:39:23 +0530

    Refcator the code

--- src/pages/app.jsx
+++ src/pages/app.jsx
@@ -6,6 +6,8 @@ import Skeleton from '@mui/material/Skeleton';
 
 import { useGetItemsQuery } from 'src/redux/services/apiSlice';
 
+import SkeletonView from 'src/components/skeletonView';
+
 import { ProductsView } from 'src/sections/products/view';
 
 // ----------------------------------------------------------------------
@@ -14,16 +16,7 @@ export default function AppPage() {
 
   const {data, isLoading} = useGetItemsQuery();
   
-  if (isLoading) return <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-  {Array.from(Array(6)).map((_, index) => (
-    <Grid item xs={2} sm={4} md={4} key={index}>
-      <Box sx={{ pt: 0.5 }}>
-        <Skeleton />
-        <Skeleton width="60%" />
-      </Box>
-    </Grid>
-  ))}
-</Grid>;
+  if (isLoading) return <SkeletonView />
 
   return (
     <>
--- src/pages/myItems.jsx
+++ src/pages/myItems.jsx
@@ -2,13 +2,15 @@ import { Helmet } from 'react-helmet-async';
 
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
-import Skeleton from '@mui/material/Skeleton';
 
 import { useAuth } from 'src/hooks/AuthProvider';
 
 import { useGetCurrentUserItemsQuery } from 'src/redux/services/apiSlice';
 
 import { ProductsView } from 'src/sections/products/view';
+
+import skeletonView from '../components/skeletonView';
+
 // ----------------------------------------------------------------------
 
 export default function ProductsPage() {
@@ -19,16 +21,7 @@ export default function ProductsPage() {
 
   const { data, isLoading} = useGetCurrentUserItemsQuery(userId);
 
-  if (isLoading) return <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-  {Array.from(Array(6)).map((_, index) => (
-    <Grid item xs={2} sm={4} md={4} key={index}>
-      <Box sx={{ pt: 0.5 }}>
-        <Skeleton />
-        <Skeleton width="60%" />
-      </Box>
-    </Grid>
-  ))}
-</Grid>;
+  if (isLoading) return <skeletonView/>;
 
   return (
     <>
